952e9ffbc617f54638ee580201caa41a614361fa
Amadeusz Chmielowski <amadeusz.chmielowski@gmail.com>
2019-05-14 22:47:25 +0200

    Added pictures
    Added simple picture functionality
    
    Signed-off-by: Amadeusz Chmielowski <amadeusz.chmielowski@gmail.com>

--- First_AndroidApp/app/src/main/java/com/amadeuszchmielowski/first_android_app/MainActivity.java
+++ First_AndroidApp/app/src/main/java/com/amadeuszchmielowski/first_android_app/MainActivity.java
@@ -5,12 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean flag = false;
+
     public void clickFunction(View view) {
+
         EditText text = findViewById(R.id.myTextField);
         EditText text2 = findViewById(R.id.editText2);
+        ImageView image = findViewById(R.id.imageView);
+        if (flag == false){
+            image.setImageResource(R.drawable.background_hdpi);
+            flag = true;
+        }
+        else{
+            image.setImageResource(R.drawable.settings);
+            flag = false;
+        }
+
+        text.setText(text2.getText().toString());
+        Toast.makeText(MainActivity.this, text2.getText().toString(), Toast.LENGTH_SHORT).show();
         Log.i("Info", text.getText().toString() + " " + text2.getText().toString());
     }
 
