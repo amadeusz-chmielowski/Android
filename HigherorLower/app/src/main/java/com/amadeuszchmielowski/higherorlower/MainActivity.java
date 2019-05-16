81d5282bd3a65a567ba9606a3af100d637a55b91
Amadeusz Chmielowski <amadeusz.chmielowski@gmail.com>
2019-05-16 19:44:55 +0200

    Running higher or lower game with fixed portrait mode.
    Signed-off-by: Amadeusz Chmielowski <amadeusz.chmielowski@gmail.com>

---
+++ HigherorLower/app/src/main/java/com/amadeuszchmielowski/higherorlower/MainActivity.java
@@ -0,0 +1,57 @@
+package com.amadeuszchmielowski.higherorlower;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int firstNumber = 0;
+    public int secondNumber = 0;
+    private int number = 0;
+
+
+    public void makeToast(String string){
+
+        Toast.makeText(MainActivity.this, string, Toast.LENGTH_LONG).show();
+    }
+    public void clicer(View view){
+        Random generator = new Random();
+        try {
+            EditText text = findViewById(R.id.numbers);
+
+            if(Integer.valueOf(text.getText().toString()) == number){
+               makeToast("Congrats!");
+                number = generator.nextInt(secondNumber)+1;
+            }
+            else if(Integer.valueOf(text.getText().toString()) > number){
+                makeToast("Higher!");
+            }
+            else{
+                makeToast("Lower");
+            }
+            text.setText("");
+        }
+        catch (Exception ex){
+            makeToast(ex.toString());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firstNumber = getResources().getInteger(R.integer.firstNumber_);
+        secondNumber = getResources().getInteger(R.integer.secondNumber_);
+        Random generator = new Random();
+        setContentView(R.layout.activity_main);
+        String text = getString(R.string.top_title, firstNumber, secondNumber);
+        TextView topTitle = findViewById(R.id.topTitle);
+        topTitle.setText(text);
+        number = generator.nextInt(secondNumber)+1;
+    }
+}
